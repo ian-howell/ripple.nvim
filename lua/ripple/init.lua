bce9a592fd9eb97c97435a032a122110ce2872c7
Ian Howell <ian.howell0@gmail.com>
2025-02-22 18:09:56 +0000

    Make the default match the opts

--- lua/ripple/init.lua
+++ lua/ripple/init.lua
@@ -1,20 +1,22 @@
 local M = {}
 
 M.setup = function(opts)
-	local defaults = {
-		expand_right = { "<C-right>", mode = { "n", "v" }, desc = "expand right" },
-		expand_left = { "<C-left>", mode = { "n", "v" }, desc = "expand left" },
-		expand_up = { "<C-up>", mode = { "n", "v" }, desc = "expand up" },
-		expand_down = { "<C-down>", mode = { "n", "v" }, desc = "expand down" },
+	local default = {
+		keys = {
+			expand_right = { "<C-right>", mode = { "n", "v" }, desc = "expand right" },
+			expand_left = { "<C-left>", mode = { "n", "v" }, desc = "expand left" },
+			expand_up = { "<C-up>", mode = { "n", "v" }, desc = "expand up" },
+			expand_down = { "<C-down>", mode = { "n", "v" }, desc = "expand down" },
+		},
 	}
 
-	local keys = vim.tbl_deep_extend("force", defaults, (opts and opts.keys) or {})
+	local keys = vim.tbl_deep_extend("force", default.keys, (opts and opts.keys) or {})
 	for func_name, args in pairs(keys) do
 		if keys[func_name] then
 			if type(args) == "string" then
-				args = vim.tbl_deep_extend("force", defaults[func_name], { args })
+				args = vim.tbl_deep_extend("force", default.keys[func_name], { args })
 			elseif type(args) == "table" then
-				args = vim.tbl_deep_extend("force", defaults[func_name], args)
+				args = vim.tbl_deep_extend("force", default.keys[func_name], args)
 			end
 			vim.keymap.set(args.mode, args[1], M[func_name], { desc = args.desc })
 		end
